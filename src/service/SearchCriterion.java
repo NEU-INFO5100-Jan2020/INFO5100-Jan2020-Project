47baac18dd1a0a9b3d1da8a6657b3eb10afe5c87
Arch Wu <archwu0817@gmail.com>
2020-04-06 11:02:59 -0700

    updated SearchCriterion with changing mandatory input to String array

--- src/service/SearchCriterion.java
+++ src/service/SearchCriterion.java
@@ -1,16 +1,16 @@
 package service;
 public class SearchCriterion {
   /*
-  SearchCriterion class serves as a template of data structure that how parameters GUIs need to pass into our services
+  SearchCriterion class serves as a template of data structure that how parameters all GUIs need to pass into our services
    */
-  String signature;
+  String[] mandatoryInput;
   String[] optionalSearchFilters;
 
-  public SearchCriterion(String signature, String[] optionalSearchFilters) {
+  public SearchCriterion(String[] mandatoryInput, String[] optionalSearchFilters) {
     /*
-    Constructor of SearchCriterion, still takes mandatory parameter and optional arguments
+    Constructor of SearchCriterion, still takes mandatory and optional parameters
      */
     this.optionalSearchFilters = optionalSearchFilters;
-    this.signature = signature;
+    this.mandatoryInput = mandatoryInput;
   }
 }
