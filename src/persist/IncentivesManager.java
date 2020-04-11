4087bd69493d100d5814a7bad8dc93a57100d42a
PoonamHingankar <37133678+PoonamHingankar@users.noreply.github.com>
2020-04-11 13:02:15 -0700

    Update IncentivesManager.java

--- src/persist/IncentivesManager.java
+++ src/persist/IncentivesManager.java
@@ -16,25 +16,12 @@ public interface IncentivesManager {
     /*Add a new entry in 'Incentives' table with the parameters as listed.
      * Returns true - if incentive is added successfully
      *        false - if there is an error in operation and incentive isn't added to the db*/
-    boolean addIncentive(String title,
-                          String description,
-                          String disclaimer,
-                          Date startDate,
-                          Date endDate,
-                          int discountValue,
-                          String discountType);
+    boolean addIncentive(Incentives incentives);
 
     /*Update the 'Incentives' table for given 'incentivesId' in  with the parameters as listed.
      * Returns true - if incentive is updated successfully
      *        false - if there is an error in operation and incentive isn't updated to the db*/
-    boolean updateIncentive(int incentivesId,
-                          String title,
-                          String description,
-                          String disclaimer,
-                          Date startDate,
-                          Date endDate,
-                          int discountValue,
-                          String discountType);
+    boolean updateIncentive(Incentives incentives);
 
     /*Delete entry from 'Incentives' table with given 'incentivesId' */
     boolean deleteIncentive(int incentivesId);
