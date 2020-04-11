bd79232a6096413f74a34978af6678af50505a1e
ekiehuang <59786138+ekiehuang@users.noreply.github.com>
2020-04-10 17:01:45 -0700

    IncentiveSearchFilter part added

--- src/service/SearchFilter.java
+++ src/service/SearchFilter.java
@@ -65,12 +65,37 @@ class VehicleSearchFilterElement extends SearchFilterElement {
 }
 
 class IncentiveSearchFilterElement extends SearchFilterElement {
-  /*
-  FilterElement for Incentive GUI: case 5, team 3
-  @Ekie may implement this class based on my implementation above
-   */
+    /*
+    FilterElement for Incentive GUI: case 5, team 3
+    @Ekie may implement this class based on my implementation above
+     */
+    public IncentiveSearchFilterElement(IncentiveSearchCriterion key, String value) {
+        this.name = key.key;
+        this.value = value;
+    }
 
+    public enum IncentiveSearchCriterion {
+        /*
+        Enum objects include all possible optional search filter of Vehicle Search GUI
+         */
+        MaxPrice("MaxPrice"),
+        MinPrice("MinPrice"),
+        New("New");
+
+        private final String key; // key is the String value of each enum element
+
+        private IncentiveSearchCriterion(String key) {
+            // private constructor that binds key string to enum element
+            this.key = key;
+        }
+
+        public String getKey() {
+            // getter of key of enum
+            return key;
+        }
+    }
 }
+
 
 class DealerSearchFilterElement extends SearchFilterElement {
   /*
@@ -117,13 +142,16 @@ class VehicleSearchFilter extends SearchFilter {
 }
 
 class IncentiveSearchFilter extends SearchFilter {
-  /*
-  SearchFilter implementation for Incentive GUI
- */
-  public IncentiveSearchFilter() {
-    super();
-  }
-}
+        /*
+        SearchFilter implementation for Incentive GUI
+       */
+        int dealerID;
+
+        public IncentiveSearchFilter(int dealerID) {
+            super();
+            this.dealerID = dealerID;
+        }
+    }
 
 class DealerSearchFilter extends SearchFilter {
   /*
