e48fd7cc70c6e9dfc40564cbfb9e44e2ddf7b262
Lakshya Kumar <kumar.lakshya24@gmail.com>
2020-04-13 13:45:24 -0700

    Test commit to verify imports for DealerSearchFilter Changed maxradius to 25 as default, API maximum default value is unknown

--- src/service/SearchFilter.java
+++ src/service/SearchFilter.java
@@ -1,7 +1,7 @@
 package service;
-
-import org.json.*;
-
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.lang.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -120,9 +120,7 @@ class DealerSearchFilterElement extends SearchFilterElement {
     Enum objects include all possible optional search filter of Dealer Search GUI
      */
     DEALERNAME("DealerName"),
-    ZIP("Zip"),
-    MINRADIUS("MinRadius"),
-    MAXRADIUS("MaxRadius");
+    ZIP("Zip");
 
     private final String key; // key is the String value of each enum element
 
@@ -187,20 +185,15 @@ class IncentiveSearchFilter extends SearchFilter {
 }
 
 class DealerSearchFilter extends SearchFilter {
-  private final int minradius = 0;
-  private final int maxradius = 100;
   private final String zip = " ";
-
   /*
     SearchFilter implementation for Dealer Search GUI
    */
   public DealerSearchFilter(String zip, int minradius, int maxradius) {
     super();
   }
-
-
-  public ArrayList<String> zipCodeRadius(String zip, int minradius, int maxradius) {
-    String url = "https://api.zip-codes.com/ZipCodesAPI.svc/1.0/FindZipCodesInRadius?zipcode=" + zip + "&minimumradius=" + minradius + "&maximumradius=" + maxradius + "&key=CKJ5LCW9PZAFNVNA8WFN";
+  public ArrayList<String> zipCodeRadius(String zip, int minradius, int maxradius) throws Exception{
+    String url = "https://api.zip-codes.com/ZipCodesAPI.svc/1.0/FindZipCodesInRadius?zipcode=" + zip + "&minimumradius=" + minradius + "&maximumradius=" + maxradius + "&key=F14RQVG8YDILP7E79JIP";
     URL obj = new URL(url);
     HttpURLConnection con = (HttpURLConnection) obj.openConnection();
     con.setRequestMethod("GET");
@@ -212,30 +205,19 @@ class DealerSearchFilter extends SearchFilter {
       response.append(inputLine);
     }
     in.close();
-    //Uncomment below code if you wish to see the raw response from the API:
-    //System.out.println(response.toString());
-    //Replace the start of the JSON o/p with blank because JSON should start with {
-    response.replace(0, 3, " ");
-    //Read JSON response and print
+    response.replace(0, 1, " ");
     JSONObject myResponse = new JSONObject(response.toString());
-    /*Within the JSON response from the API, there is a JSONArray which has different keys and their corresponding values
-     * example: "City" is a key and the value would be Bellevue*/
     JSONArray DataList = myResponse.getJSONArray("DataList");
-    //Uncomment below code to see the length of response from the API call for a particular zip code
-    //Iterating through the indexes of the Array but skipping element 0 as it is the entered zip code
     ArrayList<String> arr = new ArrayList<>();
     for (int index = 1; index < DataList.length(); index++) {
       JSONObject obj1 = DataList.getJSONObject(index);
-
-      /*The result below can be String Array, List or Collection based on preference for your GUI output*/
-      //System.out.println(obj1.getString("City") + " " + obj1.getString("Code") + " " + obj1.getString("County") + " COUNTY" + " " + obj1.getDouble("Distance"));
       String str = obj1.getString("Code");
       for (int i = 0; i < 1; i++) {
         arr.add(i, str);
+        arr.add(i, "WA" + str);
       }
     }
     Collections.reverse(arr);
-    //System.out.println(arr);
     return arr;
   }
 }
