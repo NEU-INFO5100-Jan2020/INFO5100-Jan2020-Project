32d28028535006a83ff38cef19ff1f5f05786146
Arch Wu <archwu@users.noreply.github.com>
2020-04-20 21:36:44 -0700

    Update: moved all the functionalities to methods in DealerUtilities. … (#32)
    * Update: moved all the functionalities to methods in DealerUtilities. Created tests for DealerID and VIN validator
    
    * Reduced size of data requested from DB

--- src/service/DealerUtilities.java
+++ src/service/DealerUtilities.java
@@ -1,12 +1,29 @@
 package service;
 
 
+import dto.Vehicle;
+import persist.ConnectionToSql;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class DealerUtilities {
-  MakeModelContainerV2 makeModelContainer;
-  VinValidator vinValidator;
-  DealerIDValidator dealerIDValidator;
-  ColorContainer colorContainer;
+
   public DealerUtilities(){
 
+  }
+
+  public boolean validateVin(Vehicle vehicle) throws SQLException {
+    String sqlValidationQuery = String.format("SELECT VIN FROM VehicleTable WHERE VIN = '%s' AND DealerID = '%s'", vehicle.getVin(), vehicle.getDealerId());
+    ConnectionToSql connectionToSql = new ConnectionToSql();
+    ResultSet resultSet = connectionToSql.executeValidation(sqlValidationQuery);
+    return resultSet.next();
+  }
+
+  public boolean validateDealerID(int DealerID) throws SQLException {
+    String sqlValidationQuery = String.format("SELECT DealerID FROM Dealer WHERE DealerID = '%d'", DealerID);
+    ConnectionToSql connectionToSql = new ConnectionToSql();
+    ResultSet resultSet = connectionToSql.executeValidation(sqlValidationQuery);
+    return resultSet.next();
   }
 }
