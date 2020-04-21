3fbdd37570290e1d0d55d4452738e8b4831d8daa
Swati Bhojwani <60072407+swatibh24@users.noreply.github.com>
2020-04-21 14:21:39 -0700

    Update Incentives.java
    add finalDiscountedPrice field

--- src/dto/Incentives.java
+++ src/dto/Incentives.java
@@ -17,6 +17,8 @@ public class Incentives extends BigDataType{
     private boolean isDeleted;
     private String filterList;
     private String vehicleIdList;
+    private float discountedPrice;
+
 
     public int getIncentiveId() {
         return incentiveId;
@@ -113,4 +115,12 @@ public class Incentives extends BigDataType{
     public void setVehicleIdList(String vehicleIdList) {
         this.vehicleIdList = vehicleIdList;
     }
+    
+    public float getDiscountedPrice() {
+		return discountedPrice;
+	}
+
+	public void setDiscountedPrice(float discountedPrice) {
+		this.discountedPrice = discountedPrice;
+	}
 }
